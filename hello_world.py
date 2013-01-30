29b5376dd93e85536f4eee8343b7b84a270c7fec
Kris Hyre <krish40k@gmail.com>
2013-01-30 00:09:00 -0800

    Intial Repo and Test Commit
    Just set up a GitHub acct and am gettign oriented. Created the first
    repo and am sending a commit.

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#   Initial upload to github while setting up acct
+
+def hello_world():
+    nameFirst = raw_input("What is your first name:")
+    nameLast = raw_input("What is your last name:")
+
+    print "Hello, " + nameFirst + " " + nameLast + ", it is nice to meet you."
+
+hello_world()
